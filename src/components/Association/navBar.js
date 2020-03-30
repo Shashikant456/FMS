539e99caa58b62cad78fe61ef5f2e0e4ea9d03de
shashi <shashikant.sd.98@gmail.com>
2020-03-30 15:36:22 +0530

    added

--- src/components/Association/navBar.js
+++ src/components/Association/navBar.js
@@ -8,6 +8,16 @@ import { withRouter,Link,NavLink } from 'react-router-dom'
 
 
 class Navbar extends Component {
+    state={
+        Block:true
+    }
+    componentDidMount(){
+        axios.get('/stskFmsApi/blocks/getAllBlocks')
+        .then(res =>{
+            console.log(res)
+            console.log(res.data)
+        })
+    }
     render() {
         return (
             <div className="white"> 
@@ -29,34 +39,58 @@ class Navbar extends Component {
             </nav>
           </div>
         
-            <div className="row white">
-                <div className="col s12 m10 l10 offset-m1 offset-l1 z-depth-1 ">
-                <h4>Block Details</h4>
-                    <div id="MianBlock" className="col s2 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
-                    <br></br>
-                     <div id="block" className="button center-align white"><i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Block A</div>
-                    <h6 className="center white-text"><i class="material-icons">add</i>Add flat</h6>
-                    </div>
-                    
-                    <div id="MianBlock" className="col s2 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
-                    <br></br>
-                     <div id="block" className="button center-align white"><i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Block A</div>
-                    <h6 className="center white-text"><i class="material-icons">add</i>Add flat</h6>
-                    </div>
+          <div className="row">                                                                                                                     
+          <div className="">
+               <img className="center" id="dashboard" src={dashboard} ></img>
+               </div>
+          </div>
 
-                    <div id="MianBlock" className="col s2 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
-                    <br></br>
-                     <div id="block" className="button center-align white"><i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Block A</div>
-                    <h6 className="center white-text"><i class="material-icons">add</i>Add flat</h6>
-                    </div>
-                    <div id="MianBlock" className="col s2 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
-                    <br></br>
-                     <div id="block" className="button center-align white"><i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Block A</div>
-                    <h6 className="center white-text"><i class="material-icons">add</i>Add flat</h6>
-                    </div>
+          {this.state.Block ? ( 
+        <div className="row white">
+          <div className="col s10 m10 l10">
+          <div className="col s10 m10 l10 offset-m1 offset-l1 z-depth-1">
+          <h4>Block Details</h4>
+              <div id="MianBlock" className="col s5 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
+              <br></br>
+               <div id="block" onClick={()=>this.setState({Block:false})} className="button center-align white">
+               <i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Block A</div>
+              <h6 className="center white-text"><i class="material-icons">add</i>Add flat</h6>
+              </div>
+          </div>
+          </div>
+      </div>
+      ):(
+          <div className="row">
+          <div className="col s10 m10 l10">
+          <div className="col s10 m10 l10 offset-m1 offset-l1 z-depth-1">
+            <div className="row white">
+            <div className="col s10 m10 l10">
+                <h4>Block Details</h4>
+                <div id="MianBlock" className="col s5 m2 l2 z-depth-1 red container" onClick={this.handleVendor} >
+                <br></br>
+                <div id="block" onClick={()=>this.setState({Block:true})} className="button center-align white">
+                <i class="material-icons left">keyboard_arrow_down</i><i class="material-icons right">edit</i>Block A</div>
+                <br></br>
                 </div>
             </div>
-            
+            </div>
+
+            <div className="row white">
+            <div className="col s12 m10 l10">
+                <h4>Block A- Flore Details</h4>
+                <div id="MianBlock" className="col s4 m2 l2 z-depth-1 grey container" onClick={this.handleVendor} >
+                <br></br>
+                <div id="block" onClick={()=>this.setState({Block:true})} className="button center-align white">
+                <i class="material-icons left">keyboard_arrow_right</i><i class="material-icons right">edit</i>Ground Flore</div>
+                <h6 className="center white-text"><i class="material-icons">add</i>Add a room</h6>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+</div>
+     )}
+           
 
         </div>
         )
